debad5866699b9947aff5fc3eca0d4638f5d6f34
reactexcel <rohit.excel2011@gmail.com>
2020-10-10 07:19:10 +0530

    Form and Field component added

--- src/components/YouTubeForm.jsx
+++ src/components/YouTubeForm.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useFormik } from "formik";
+import { Formik, Form, Field } from "formik";
 
 function YoutubeForm() {
   //intialValues field name,corresponds to name attributes of individual field
@@ -40,26 +40,19 @@ function YoutubeForm() {
     return errors;
   };
 
-  const formik = useFormik({
-    initialValues,
-    onSubmit,
-    validate,
-  });
-
   console.log(formik.values, "FOrm data");
   console.log(formik.errors, "Errors Data");
   console.log(formik.touched, "Fields visited");
   return (
-    <div>
-      <form onSubmit={formik.handleSubmit}>
+    <Formik
+      initialValues={initialValues}
+      onSubmit={onSubmit}
+      validate={validate}
+    >
+      <Form>
         <div className='form-control'>
           <label htmlFor='name'>name</label>
-          <input
-            type='text'
-            id='name'
-            name='name'
-            {...formik.getFieldProps("name")}
-          />
+          <Field type='text' id='name' name='name' />
           {formik.touched.name && formik.errors.name ? (
             <div className='error'>{formik.errors.name}</div>
           ) : null}
@@ -67,31 +60,21 @@ function YoutubeForm() {
 
         <div className='form-control'>
           <label htmlFor='email'>E-mail</label>
-          <input
-            type='email'
-            id='email'
-            name='email'
-            {...formik.getFieldProps("email")}
-          />
+          <Field type='email' id='email' name='email' />
           {formik.touched.email && formik.errors.email ? (
             <div className='error'>{formik.errors.email}</div>
           ) : null}
         </div>
         <div className='form-control'>
           <label htmlFor='channel'>Channel</label>
-          <input
-            type='text'
-            id='channel'
-            name='channel'
-            {...formik.getFieldProps("channel")}
-          />
+          <Field type='text' id='channel' name='channel' />
           {formik.touched.channel && formik.errors.channel ? (
             <div className='error'>{formik.errors.channel}</div>
           ) : null}
           <button type='submit'>Submit</button>
         </div>
-      </form>
-    </div>
+      </Form>
+    </Formik>
   );
 }
 
@@ -101,3 +84,17 @@ export default YoutubeForm;
 //Formik component :Formik,Form,Field,ErrorMessage
 //Formik component is replace for useFormik hook ..the object we pass to useFormik hook
 //will be passed to Formik component as props
+
+//Formik component behave like context provided component that provides properties and helper methods
+//for other component
+//steps: 1-import Formik 2-pass it desired prosp 3-import Form 4-replace html form with Form component
+//Form component automatically hooks(pass) "handleSubmit" to "onSubmit" method
+// Form Component automatically links "onSubmit" method with "handleSubmit"
+
+//Field component simplify code for form field
+//right now we were passing getFieldsProps to every field
+
+//steps: 1-import Field ,2-replace input TAG  with Field component(we don't need getFieldProps now)
+//Field component does three things 1-it hooks up input to Top level Formik component
+// 2-It use name attribute to  matchup with the formik state
+// 3-Default it renders a input Field
