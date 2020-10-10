7b034e6fcb6310e718a6504ee44be94485e625c6
reactexcel <rohit.excel2011@gmail.com>
2020-10-10 06:57:51 +0530

    Formik component to replace useFormik hooks

--- src/components/YouTubeForm.jsx
+++ src/components/YouTubeForm.jsx
@@ -97,48 +97,7 @@ function YoutubeForm() {
 
 export default YoutubeForm;
 
-//useformik hooks help in mananging form state,Handling form submission,validation and error messages
-//useformik is a hook(function) ,we need to call it in our component
-//it takes an object as parameter...This hook return object which contain varity of useful properties
-//and methods which can be used with our form
-//we can give any name to return object
-//we do everything with this returned object
-
-//when we write something in form the value of the field changes..we normally manage this through react state
-//example we need this state for name,email and channel name ....this is called form state
-//form state is a object which manage all form changes
-
-//step 1:pass a intialvalue(props) in the object we pass to useformik hook
-//initalValues property is an object
-
-//step2 :add onchange and value props to each of field value(this ensures that form fields in react is tracks by "formik")
-//useformik returned object have a lot of useful methods
-//for onChange, formik have "formik.handleChange"
-//for values ,formik have "formik.values.name"
-//after this formik will automatically manage and track form field values for you...
-
-//     value={formik.values.channel} ...here channel refer to name attribute in input field
-
-//valiadtion......a validation function need to be defined to a validate function which altumatically gets values OBJECT as it's argument
-//in the object that we pass to useFormik hook
-
-//$$$$ validate function should satisfy some conditions for formik to work as intended
-
-//1.this validate function should return a object
-//eg: validate :values=>{
-//     let errors={}
-
-//     return errors;
-// }
-//2:Keys of the error object should be similar to that of values object && also key corresponds to name attribute for form field
-
-//3:VALUES of that key should be a string indicating what the error message should be for that particular field
-
-//"formik.errors" is a error objects with validations
-
-//formik runs the validate function "onChange" of the form .,then populate "formik.errors" object
-
-//for checking if a user have visted any field and show error for that specific field
-//we need "onBlur" props
-
-//formik stores the visted field in "touched" object
+//formik provides few component that uses "react context" internally ,and make code less verbose(remove boiler plate)
+//Formik component :Formik,Form,Field,ErrorMessage
+//Formik component is replace for useFormik hook ..the object we pass to useFormik hook
+//will be passed to Formik component as props
