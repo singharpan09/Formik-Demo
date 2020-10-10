bf6d443885fd4e9283b079347f481d413f84045b
reactexcel <rohit.excel2011@gmail.com>
2020-10-10 07:30:33 +0530

    Error Message compoent added

--- src/components/YouTubeForm.jsx
+++ src/components/YouTubeForm.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Formik, Form, Field } from "formik";
+import { Formik, Form, Field, ErrorMessage } from "formik";
 
 function YoutubeForm() {
   //intialValues field name,corresponds to name attributes of individual field
@@ -40,9 +40,6 @@ function YoutubeForm() {
     return errors;
   };
 
-  console.log(formik.values, "FOrm data");
-  console.log(formik.errors, "Errors Data");
-  console.log(formik.touched, "Fields visited");
   return (
     <Formik
       initialValues={initialValues}
@@ -53,24 +50,18 @@ function YoutubeForm() {
         <div className='form-control'>
           <label htmlFor='name'>name</label>
           <Field type='text' id='name' name='name' />
-          {formik.touched.name && formik.errors.name ? (
-            <div className='error'>{formik.errors.name}</div>
-          ) : null}
+          <ErrorMessage name='name' />
         </div>
 
         <div className='form-control'>
           <label htmlFor='email'>E-mail</label>
           <Field type='email' id='email' name='email' />
-          {formik.touched.email && formik.errors.email ? (
-            <div className='error'>{formik.errors.email}</div>
-          ) : null}
+          <ErrorMessage name='email' />
         </div>
         <div className='form-control'>
           <label htmlFor='channel'>Channel</label>
           <Field type='text' id='channel' name='channel' />
-          {formik.touched.channel && formik.errors.channel ? (
-            <div className='error'>{formik.errors.channel}</div>
-          ) : null}
+          <ErrorMessage name='channel' />
           <button type='submit'>Submit</button>
         </div>
       </Form>
@@ -98,3 +89,7 @@ export default YoutubeForm;
 //Field component does three things 1-it hooks up input to Top level Formik component
 // 2-It use name attribute to  matchup with the formik state
 // 3-Default it renders a input Field
+
+//ErrorMessage component steps 1:import ErrorMessage 2-use Error Component and pass it name props
+//which is equal to name attribute for Field component
+//ErrorMessage take care of error message and show error if a field is visted and have error message
